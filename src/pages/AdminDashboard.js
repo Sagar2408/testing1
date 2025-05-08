734a06548526fe2064dd0e1410a3ff9739a32642
Sagar <sagar90160@gmail.com>
2025-05-08 15:08:40 +0530

    ok

--- src/pages/AdminDashboard.js
+++ src/pages/AdminDashboard.js
@@ -4,13 +4,15 @@ import { useNavigate } from "react-router-dom";
 
 const AdminDashboard = () => {
   const navigate = useNavigate();
+
   return (
     <div>
       <Navbar />
       <h2>Admin Dashboard</h2>
-      <button onClick={() => navigate("/admin/view/cast")}>View Screen</button>
-      <button onClick={() => navigate("/admin/view/video")}>View Video</button>
-      <button onClick={() => navigate("/admin/view/audio")}>Listen Audio</button>
+      
+      <button onClick={() => navigate("/viewer?type=screen")}>View Screen</button>
+      <button onClick={() => navigate("/viewer?type=video")}>View Video</button>
+      <button onClick={() => navigate("/viewer?type=audio")}>Listen Audio</button>
     </div>
   );
 };
